31db733c704e216a4c8db5262c6b71118f9e38f6
vrundabhujbal <vrundabhujbal7@gmail.com>
2018-07-10 19:14:02 +0530

    My Django Girls app, first commit

---
+++ empenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\vrunda bhujbal\desktop\python advanced\django-project\empenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Vrunda Bhujbal\Desktop\Python advanced\Django-project\myenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
